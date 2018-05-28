7831355cda302f57dceea92d1cb7c0bc9b511dd4
Mauricio Perea Rodríguez <mperea@portalteck.com>
2018-05-27 20:31:38 -0500

    Subida de archivos

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 app.use(require('./categoria'));
+app.use(require('./imagenes'));
 app.use(require('./login'));
 app.use(require('./producto'));
+app.use(require('./upload'));
 app.use(require('./usuario'));
 
 module.exports = app;
